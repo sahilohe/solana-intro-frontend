b34b69ce1935818f0a013307fd4ea3730f5d0edf
sahilohe <sahilohe97@gmail.com>
2023-06-14 11:11:35 +0530

    implemented isExecutable

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,34 @@
 import type { NextPage } from 'next'
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 import styles from '../styles/Home.module.css'
 import AddressForm from '../components/AddressForm'
 import * as Web3 from '@solana/web3.js'
 
 const Home: NextPage = () => {
-  const [balance, setBalance] = useState(0)
-  const [address, setAddress] = useState('')
+  const [balance, setBalance] = useState(0);
+  const [address, setAddress] = useState('');
+  const [isExecutable, setIsExecutable] = useState('');
 
-  const addressSubmittedHandler = (address: string) => {
+  const addressSubmittedHandler =  async (address: string) => {
     try {
       setAddress(address)
-      const key = new Web3.PublicKey(address)
-      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'))
-      connection.getBalance(key).then(balance => {
-        setBalance(balance / Web3.LAMPORTS_PER_SOL)
+      const key = new Web3.PublicKey(address);
+      const connection = new Web3.Connection(Web3.clusterApiUrl('devnet'));
+      await connection.getBalance(key).then(balance => {
+        setBalance(balance / Web3.LAMPORTS_PER_SOL);
+      });
+
+      await connection.getAccountInfo(key).then(accountInfo => {
+        if (accountInfo !== null) {
+          setIsExecutable(`${accountInfo.executable}`);
+        } else {
+          setIsExecutable('');
+        }
       })
+      
     } catch (error) {
-      setAddress('')
-      setBalance(0)
+      setAddress('');
+      setBalance(0);
       alert(error)
     }
   }
@@ -32,6 +42,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Executable: ${isExecutable}`}</p>
       </header>
     </div>
   )
